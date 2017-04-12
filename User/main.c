2e5bc7393c73428987224e4a5113c95e91a673c1
Magnism <maggnism@gmail.com>
2017-04-12 10:26:27 +0200

    ajout pin config buzzer et memoire

--- User/main.c
+++ User/main.c
@@ -19,7 +19,9 @@
 #include "globaldec.h" // fichier contenant toutes les déclarations de variables globales
 #include <stdio.h>
 
-	
+void spi_init();
+void pin_Configuration();
+
 
 //===========================================================//
 // Function: Main
@@ -39,6 +41,7 @@ int main(void)
 	  dessiner_rect(120,170,110,110,2,1,Black,Red);
 		
 	  touch_init(); // init pinsel tactile et init tactile 
+		pin_Configuration();
 	  
     while(1)  ; 
 	// pour l'instant, le main fait juste quelques inits ...
@@ -47,7 +50,54 @@ int main(void)
 		
 		
 		
-	}
+}
+
+void pin_Configuration()
+{
+	/*
+	 * Initialize pin connect
+	 */
+  PINSEL_CFG_Type speaker_init_cfg;
+	PINSEL_CFG_Type memory_init_cfg;
+	PINSEL_CFG_Type memory_init_cfg2;
+	
+	
+	///// SPEAKER /////
+	speaker_init_cfg.Funcnum = PINSEL_FUNC_0;	//AS GPIO
+	speaker_init_cfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+	speaker_init_cfg.Pinmode = PINSEL_PINMODE_PULLUP;
+	
+	
+	speaker_init_cfg.Pinnum = PINSEL_PIN_9;          
+	speaker_init_cfg.Portnum = PINSEL_PORT_1; //P1.9 = speaker pin
+  PINSEL_ConfigPin(&speaker_init_cfg);
+
+	
+	GPIO_SetDir (1,(1<<9),1);  // PORT1.9 defined as output   (port, pin, output)
+
+  FIO_SetDir(1,(1<<9),1);
+  FIO_SetValue(1,(1<<9));// ecrire 1 => P1.9
+	
+	
+	///// MEMORY /////
+	memory_init_cfg.Funcnum = PINSEL_FUNC_1;	//AS GPIO
+	memory_init_cfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+	memory_init_cfg.Pinmode = PINSEL_PINMODE_PULLUP;
+	
+	
+	memory_init_cfg.Pinnum = PINSEL_PIN_27;
+	memory_init_cfg.Portnum = PINSEL_PORT_0; //P0.27 = memory
+  PINSEL_ConfigPin(&memory_init_cfg);
+	
+	
+	memory_init_cfg2.Funcnum = PINSEL_FUNC_1;	//AS GPIO
+	memory_init_cfg2.OpenDrain = PINSEL_PINMODE_NORMAL;
+	memory_init_cfg2.Pinmode = PINSEL_PINMODE_PULLUP;
+	
+	memory_init_cfg2.Pinnum = PINSEL_PIN_28;
+	memory_init_cfg2.Portnum = PINSEL_PORT_0; //P0.28 = memory
+  PINSEL_ConfigPin(&memory_init_cfg2);
+}
 
 //---------------------------------------------------------------------------------------------	
 #ifdef  DEBUG
