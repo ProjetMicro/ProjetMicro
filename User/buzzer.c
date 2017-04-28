b705a840ecd62c59990a7c7cc285527083a3fc82
Magnism <maggnism@gmail.com>
2017-04-28 18:22:35 +0200

    Add files via upload

--- User/buzzer.c
+++ User/buzzer.c
@@ -27,14 +27,43 @@ void buzzer_init()
 
   FIO_SetDir(1,(1<<9),1);
   FIO_SetValue(1,(1<<9));// ecrire 1 => P1.9
-	
+
+
 	// ===== timer init ===== //
 	T0_Init();
 }
 
 void T0_Init()
 {
+	TIM_TIMERCFG_Type timerConf;
+	TIM_MATCHCFG_Type matchConf;
+
+	timerConf.PrescaleOption = TIM_PRESCALE_USVAL; //prescale en microsecondes
+	timerConf.PrescaleValue = 1; //precision de 1 us.
+
+	matchConf.ResetOnMatch = ENABLE;
+	matchConf.IntOnMatch = ENABLE;
+	matchConf.ExtMatchOutputType = TIM_EXTMATCH_TOGGLE;
+
+	NVIC_SetPriority(TIMER0_IRQn, ((0x01<<3)|0x01));
+	NVIC_EnableIRQ(TIMER0_IRQn); //Enable interrupt for timer 0
 	
+	
+	TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &timerConf); //TIMER0
+	TIM_ConfigStructInit(TIM_TIMER_MODE, &timerConf);
+	TIM_ConfigMatch(LPC_TIM0, &matchConf);
+	TIM_UpdateMatchValue(LPC_TIM0, 0, 10000);//match0 value to 50
+	//start
+	TIM_Cmd(LPC_TIM0, ENABLE);
+}
+
+void TIMER0_IRQHandler()
+{
+	signValue = !signValue;
+	FIO_SetValue(1, (signValue<<9));
+
+	//Acquittement
+	TIM_ClearIntPending(LPC_TIM0, TIM_MR0_INT);
 }
 
 int freqNote(int fb,int oc){
