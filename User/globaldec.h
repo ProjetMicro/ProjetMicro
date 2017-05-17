a1167356d6e94017d7efb57e36a3068cc0cad6ac
lukijo <mickae56@yahoo.fr>
2017-05-17 16:12:55 +0200

    memory variable

--- User/globaldec.h
+++ User/globaldec.h
@@ -5,11 +5,14 @@
 #include <stdint.h>
 #include "lpc17xx_i2c.h"
 
-// mettez ici toutes les déclarations de variables globales
+// mettez ici toutes les dÃ©clarations de variables globales
 
 ////////// ===== AFFICHAGE ===== //////////
 char chaine[30]; // buffer pour l'affichage sur le LCD
 uint16_t touch_x, touch_y ;
+
+////////// ===== MEMORY ===== //////////
+int complete_M;
 
 ////////// ===== BUZZER ===== //////////
 int etatSon = 0;
