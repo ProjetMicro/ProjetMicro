0b0a04bb7ab41d868388344793a52d1b129f7f86
lukijo <mickae56@yahoo.fr>
2017-05-17 16:25:16 +0200

    memory h

--- User/memory.h
+++ User/memory.h
@@ -2,11 +2,12 @@
 #include "lpc17xx_pinsel.h"
 
 /*
-Fonction permettant de dessiner un rectangle de coordonnées (x,y)
-de longueur lng, de largeur lrg, d'épaisseur de trait de bordure e,
+Fonction permettant de dessiner un rectangle de coordonnÃ©es (x,y)
+de longueur lng, de largeur lrg, d'Ã©paisseur de trait de bordure e,
 plein ou vide, de couleur de bordure e_color et de fond bg_color
 */
-void memory_init();
+void memory_init(void);
 void init_i2c_eeprom(void);
 void i2c_eeprom_write(uint16_t, void*, int);
 void i2c_eeprom_read(uint16_t, void*, int);
+void I2C0_IRQHandler(void);
