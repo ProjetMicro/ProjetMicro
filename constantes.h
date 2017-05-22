2ce55a7f17c5546a6f36d0001ffcc0be671aec66
Magnism <maggnism@gmail.com>
2017-05-22 16:58:18 +0200

    Commentaires nettoyés + suppr variables inutiles

---
+++ constantes.h
@@ -0,0 +1,32 @@
+#ifndef CONSTANTE_H
+#define CONSTANTE_H
+
+// mettez dans ce fichier toutes les constantes et définitions 
+// des nouveaux types que vous souhaitez
+
+typedef enum {
+		NOTOUCH,
+		JAUNE,
+		VERT,
+		ROUGE,
+		BLEU
+} Touche;
+
+#define NB_TOUCHE 4
+
+/* Les constantes des fréquences de base (en Hertz) */
+#define DO 262 // Do normal
+#define DOD 277 // Do diese
+#define RE 294  // Re normal
+#define RED 311  // Re diese
+#define MI 330  // ...
+#define FA 349
+#define FAD 370
+#define SO 392
+#define SOD 415
+#define LA 440
+#define LAD 466
+#define SI 494
+
+/* precision souhaitee sur frequence pour oreil normal */
+#endif
---
+++ global.h
@@ -0,0 +1,33 @@
+#include "constantes.h" // fichier contenant toutes les constantes du projet
+#include <stdint.h>
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+// mettez ici toutes les "extern" correspondant aux déclarations contenues dans globadec.h
+
+////////// ===== AFFICHAGE ===== //////////
+// buffer pour l'affichage sur le LCD
+extern char chaine[30];
+extern uint16_t touch_x, touch_y;
+extern int flagtacheclavier;
+extern int n;
+
+////////// ===== MEMORY ===== //////////
+extern int complete_M;
+
+////////// ===== BUZZER ===== //////////
+extern int etatSon;
+extern int etatBuzzer;
+extern int us_periodSound;
+extern int us_noteDuration;
+extern int microSeconds;
+extern int microSeconds2;
+
+extern int frequTouches[NB_TOUCHE];
+
+////////// ===== JEU ===== //////////
+extern Touche jeu[10];
+extern int posJeu;
+extern int flag_jeu;
+#endif
